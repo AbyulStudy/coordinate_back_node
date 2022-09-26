198e63d0ac976b76b6ee936c4b8007fdebb9ade0
Abyul <kinsng1599@gmail.com>
2022-09-26 18:29:39 +0900

    :world_map:Route: proj4 & epsg

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,9 +1,22 @@
 import express from "express";
+import epsgController from "src/controller/epsg";
+import proj4 from "src/controller/proj4";
+import { coordinateFile } from "src/middleware/uploadHandler";
 
 import indexController from "../controller/index";
 
 const router = express.Router();
 
 router.get("/", indexController.indexPage);
+
+/**
+ * http://{serverDomain}/proj4/coordinate?separator=""&coordinateSystemType=""
+ *  */
+router.get("/proj4/coordinate", coordinateFile.single("coordinateFile"), proj4.proj4Swap);
+
+/**
+ * http://{serverDomain}/epsg?epsg=""
+ *  */
+router.get("/epsg", epsgController.epsg);
 
 export = router;
