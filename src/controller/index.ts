a14a888f2e968b8d02ce706692f916e22d4cf3ea
Abyul <kinsng1599@gmail.com>
2022-09-26 18:29:05 +0900

    :memo:Refactor: 초기화면 변경

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,7 +1,15 @@
+import ApiResponse from "src/utility/apiResponse";
 import IController from "../interface/IController";
+import ApiError from "src/utility/apiError";
 
 export default class indexController {
   static indexPage: IController = async (req, res) => {
-    res.send("AI Sucess");
+    try {
+      ApiResponse.init(res);
+    } catch (error: any) {
+      console.log(error);
+      ApiError.regist(error);
+      ApiResponse.error(res, error);
+    }
   };
 }
