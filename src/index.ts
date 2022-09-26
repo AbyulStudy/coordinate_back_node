db3638eaf809166c9722c7b9a82ee76df94886d2
Abyul <kinsng1599@gmail.com>
2022-09-26 18:30:18 +0900

    :memo:Refactor: mode 분기

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import app from "./config/express";
-import "src/config/database";
+// import "src/config/database";
 import { logger } from "src/config/logger";
 import { Config } from "src/config/config";
 
 const port = Config.server.port || 4500;
+const mode = Config.server.mode || process.env.NODE_MODE;
 
 app.listen(port, async () => {
   logger.info(`Server listening on port : ${port}`);
-  logger.info(`Server MODE : ${Config.server.mode}`);
+  logger.info(`Server MODE : ${mode}`);
 });
