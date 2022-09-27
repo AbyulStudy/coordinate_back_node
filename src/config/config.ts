fc57c758aa1ffc51f7028c6566d114ad2bc42c41
abyul <kinsng1599@gmail.com>
2022-09-27 21:38:56 +0900

    :books:config: front Config update

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,27 +3,19 @@ config({ path: ".env.public" });
 
 export const Config: any = {
   server: {
-    host: process.env.SERVER_HOST,
     port: Number(process.env.SERVER_PORT),
     mode: process.env.NODE_MODE,
     modeDev: process.env.NODE_MODE_DEV,
     modePro: process.env.NODE_MODE_PRO,
     swagger: "on"
   },
+  front: {
+    public: process.env.FRONT_PUBLIC,
+    local: process.env.FRONT_LOCAL
+  },
   fileupload: {
     coordinateDirname: process.env.FILE_COORDINATEDIR,
     maxsize: process.env.FILE_MAXSIZE,
     description: process.env.FILE_MAXSIZE_DESC
-  },
-  db: [
-    {
-      name: process.env.DB_NAME!,
-      host: process.env.DB_HOST!,
-      user: process.env.DB_USER!,
-      port: Number(process.env.DB_PORT),
-      password: process.env.DB_PASS!,
-      database: process.env.DB_DATABASE!,
-      connectionLimit: Number(process.env.DB_CONNECTIONLIMIT)
-    }
-  ]
+  }
 };
