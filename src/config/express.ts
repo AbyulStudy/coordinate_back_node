f9453184afa79b47f69c83f62e1c987ea1cb1890
abyul <kinsng1599@gmail.com>
2022-09-27 21:39:41 +0900

    :books:config: express cors update

--- src/config/express.ts
+++ src/config/express.ts
@@ -3,17 +3,31 @@ import path from "path";
 import morgan from "morgan";
 import YAML from "yamljs";
 import swaggerUi from "swagger-ui-express";
+import cors, { CorsOptions } from "cors";
 import { Config } from "src/config/config";
 import { logger, stream } from "src/config/logger";
 import indexRotuer from "src/route/index";
+import { StatusCodes } from "http-status-codes";
 
 const app = express();
-
+const origin = Config.server.mode === "development" ? Config.front.local : Config.front.public;
 /**
  * express JSON & URLENCODED
  */
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: false }));
+
+/**
+ * Cors
+ */
+const corsOption: CorsOptions = {
+  origin: origin,
+  methods: "GET,POST",
+  preflightContinue: true,
+  maxAge: 3600,
+  optionsSuccessStatus: StatusCodes.OK
+};
+app.use(cors(corsOption));
 
 /**
  * Logger [ morgan ]
